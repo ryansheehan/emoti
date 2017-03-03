4c6c7c9f123b0c905a23194f88fdd13adcf91276
Sheehan, Ryan <ryan.sheehan@tylertech.com>
2017-03-02 18:17:32 -0600

    conditional routing example

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import store from '../store';
 import Hello from 'layout/hello.layout.vue';
 import CounterLayout from 'layout/counter.layout.vue';
 
+
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -15,7 +17,20 @@ export default new Router({
     {
       path: '/counter',
       name: 'counter',
-      component: CounterLayout
+      component: CounterLayout,
+      meta: { requiresAuth: true }
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {  
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if(!store.getters.isAuthenticated) {   
+      console.warn('Route requires authentication and user is not authenticated.  set meta: { requiresAuth: false}');
+      return next('/');
+    }
+  } 
+  next();
+})
+
+export default router;
