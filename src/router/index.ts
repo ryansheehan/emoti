9a76a131364a2fc21227249c954004da12bfe770
Sheehan <ryan.sheehan@tylertech.com>
2017-03-08 13:49:29 -0600

    fix router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,8 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import store from '../store';
-import Hello from 'layout/hello.layout.vue';
-import CounterLayout from 'layout/counter.layout.vue';
-import Home from '../components/home.component.vue';
-import Login from '../components/login.component.vue';
+import Vue from "vue";
+import Router from "vue-router";
+import store from "../store";
+import Home from "../components/home.component.vue";
+import Login from "../components/login.component.vue";
 
 
 Vue.use(Router);
@@ -12,24 +10,25 @@ Vue.use(Router);
 const router = new Router({
   routes: [
     {
-      path: '/',
-      redirect: { name: 'home' }
+      path: "/",
+      redirect: { name: "home" }
     },
     {
-      path: '/home',
-      name: 'home',
+      path: "/home",
+      name: "home",
       component: Home,
       meta: { requiresAuth: true }
     },
     {
-        path: '/login',
-        name: 'login',
+        path: "/login",
+        name: "login",
         component: Login,
-        beforeEnter(to, from, next) {
-            return store.getters.isAuthenticated.then((authenticated:boolean)=>{
+        beforeEnter(to: Router.Route, from: Router.Route, next: (to?:Router.RawLocation)=>any): any {
+            return store.getters.isAuthenticated.then((authenticated:boolean)=> {
                 if(authenticated) {
-                    next({name: 'home'});
+                    return next({name: "home"});
                 }
+                return next();
             });
         }
     }
@@ -37,18 +36,15 @@ const router = new Router({
 });
 
 router.beforeEach((to, from, next) => {
-  if(to.matched.some(record => record.meta.requiresAuth)) {
-    return store.getters.isAuthenticated.then((authenticated:boolean) => {
-        if(authenticated) {
-            console.log(`User is authorized for ${to.name}.`);
-            next();
-        } else {
-            console.warn(`Route ${to.name} requires authentication and user is not authenticated.  set meta: { requiresAuth: false}`);
-            next({name: 'login'});
-        }
-    });
-  }
-  next();
+    if(to.matched.some(record => record.meta.requiresAuth)) {
+        return store.getters.isAuthenticated.then((authenticated: boolean) => {
+            if(authenticated) {
+                return next();
+            }
+            return next({name: "login"});
+        });
+    }
+    return next();
 })
 
 export default router;
