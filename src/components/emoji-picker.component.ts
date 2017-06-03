05ea93134f456778653df241bf37ecfdb65db25b
Ryan Sheehan <rsheehan@gmail.com>
2017-06-02 21:44:21 -0500

    showing all fonts

--- src/components/emoji-picker.component.ts
+++ src/components/emoji-picker.component.ts
@@ -1,13 +1,21 @@
 import Vue from "vue";
-import { Component, Prop, Watch } from "./vue-class-helpers";
+import { Component, Prop, Watch, Inject } from "./vue-class-helpers";
 // import { debounce } from "debounce";
-import { searchShortnames } from "../emojione.search";
+import { searchShortnames, searchCategories } from "../emojione.search";
 import * as emojione from "emojione";
 import "../../node_modules/emojione/extras/css/emojione.min.css"
 
 interface IDebouncedFunction extends Function {
     clear():void;
 }
+
+const iconsByCategory:{[category:string]:string[]} =
+    Object.keys(searchCategories)
+    .reduce<any>((result:{[category:string]:string[]}, category:string)=>{
+        const shortnames:string[] = searchCategories[category];
+        result[category] = shortnames.map((shortname:string)=>emojione.shortnameToImage(shortname));
+        return result;
+    }, {});
 
 @Component
 export default class EmojiPicker extends Vue {
@@ -19,6 +27,19 @@ export default class EmojiPicker extends Vue {
     // toggleShow() {
     //     this.show = !this.show;
     // }
+
+    get people():string[] { return iconsByCategory["people"]; }
+    get symbols():string[] { return iconsByCategory["symbols"]; }
+    get objects():string[] { return iconsByCategory["objects"]; }
+    get nature():string[] { return iconsByCategory["nature"]; }
+    get food():string[] { return iconsByCategory["food"]; }
+    get travel():string[] { return iconsByCategory["travel"]; }
+    get activity():string[] { return iconsByCategory["activity"]; }
+    get flags():string[] { return iconsByCategory["flags"]; }
+    get regional():string[] { return iconsByCategory["regional"]; }
+    get modifier():string[] { return iconsByCategory["modifier"]; }
+
+    searchCategories:{[category:string]:string[]} = iconsByCategory;
 
     searchTerm:string = "";
 
--- src/sfc.d.ts
+++ src/sfc.d.ts
@@ -8,10 +8,6 @@ declare module "*.json" {
     export default value;
 }
 
-declare module "debounce" {
-    export function debounce<A extends Function>(f: A, interval?: number, immediate?: boolean): A & { clear(): void; };
-}
-
 declare module "emojione" {
   export let emojiVersion:string;
   export let emojiSize:string;
@@ -33,134 +29,38 @@ declare module "emojione" {
 declare module "vue-material" {
     import Vue from 'vue';
 
-    export class MdCore {
-
-    }
-
-    export class MdAvatar {
-
-    }
-
-    export class MdBackdrop {
-
-    }
-
-    export class MdBottomBar {
-
-    }
-
-    export class MdButton {
-
-    }
-
-    export class MdButtonToggle {
-
-    }
-
-    export class MdCard {
-
-    }
-
-    export class MdCheckbox {
-
-    }
-
-    export class MdChips {
-
-    }
-
-    export class MdDialog {
-
-    }
-
-    export class MdDivider {
-
-    }
-
-    export class MdFile {
-
-    }
-
-    export class MdIcon {
-
-    }
-
-    export class MdImage {
-
-    }
-
-    export class MdInputContainer {
-
-    }
-
-    export class MdLayout {
-
-    }
-
-    export class MdList {
-
-    }
-
-    export class MdMenu {
-
-    }
-
-    export class MdProgress {
-
-    }
-
-    export class MdRadio {
-
-    }
-
-    export class MdSelect {
-
-    }
-
-    export class MdSidenav {
-
-    }
-
-    export class MdSnackbar {
-
-    }
-
-    export class MdSpeedDial {
-
-    }
-
-    export class MdSpinner {
-
-    }
-
-    export class MdSubheader {
-
-    }
-
-    export class MdSwitch {
-
-    }
-
-    export class MdTable {
-
-    }
-
-    export class MdTabs {
-
-    }
-
-    export class MdToolbar {
-
-    }
-
-    export class MdTooltip {
-
-    }
-
-    export class MdWhiteframe {
-
-    }
-
+    export class MdCore {}
+    export class MdAvatar {}
+    export class MdBackdrop {}
+    export class MdBottomBar {}
+    export class MdButton {}
+    export class MdButtonToggle {}
+    export class MdCard {}
+    export class MdCheckbox {}
+    export class MdChips {}
+    export class MdDialog {}
+    export class MdDivider {}
+    export class MdFile {}
+    export class MdIcon {}
+    export class MdImage {}
+    export class MdInputContainer {}
+    export class MdLayout {}
+    export class MdList {}
+    export class MdMenu {}
+    export class MdProgress {}
+    export class MdRadio {}
+    export class MdSelect {}
+    export class MdSidenav {}
+    export class MdSnackbar {}
+    export class MdSpeedDial {}
+    export class MdSpinner {}
+    export class MdSubheader {}
+    export class MdSwitch {}
+    export class MdTable {}
+    export class MdTabs {}
+    export class MdToolbar {}
+    export class MdTooltip {}
+    export class MdWhiteframe {}
 
     export interface IVueMaterial {
         install(Vue: Vue):void,
@@ -202,51 +102,3 @@ declare module "vue-material" {
 
     export default options;
 }
-
-declare module "vuefire" {
-  import _Vue from 'vue';
-  import {
-    database
-  } from 'firebase';
-
-  type FirebaseSnapshot = database.DataSnapshot;
-  type FirebaseReference = database.Reference;
-  type FirebaseQuery = database.Query;
-
-  // Returns the key of a Firebase snapshot across SDK versions.
-  export function _getKey (snapshot:FirebaseSnapshot): string|null;
-
-  // Returns the original reference of a Firebase reference or query across SDK versions.
-  export function _getRef (refOrQuery:FirebaseReference|FirebaseQuery): FirebaseReference;
-
-  // Check if a value is an object.
-  export function isObject(val: any): boolean;
-
-  // Convert firebase snapshot into a bindable data record.
-  export function createRecord(snapshot: FirebaseSnapshot): Object;
-
-  // Find the index for an object with given key.
-  export function indexForKey(array: Array<any>, key: string): number;
-
-  // Bind a firebase data source to a key on a vm.
-  export function bind(vm: _Vue, key: string, source: Object): void;
-
-  // Define a reactive property in a given vm if it's not defined yet
-  export function defineReactive(vm: _Vue, key: string, val: any): void;
-
-  // Bind a firebase data source to a key on a vm as an Array.
-  export function bindAsArray(vm: _Vue, key: string, source: Object, cancelCallback:Function|null|undefined): void;
-
-  // Bind a firebase data source to a key on a vm as an Object.
-  export function bindAsObject(vm: _Vue, key: string, source: Object, cancelCallback:Function|null|undefined): void;
-
-  // Unbind a firebase-bound key from a vm.
-  export function unbind(vm: _Vue, key:string): void;
-
-  // Ensure the related bookkeeping variables on an instance.
-  export function ensureRefs(vm: _Vue): void;
-
-  // Install function passed to Vue.use() in manual installation.
-  export function install(Vue: typeof _Vue): void;
-}
-
